ad413316a05e58349abebc30131971907a075172
pescel <28l.pesce@gmail.com>
2016-10-25 11:02:26 -0600

    Add upvote button functions

--- javascript.js
+++ javascript.js
@@ -2,17 +2,20 @@ var title = $('.input-title');
 var body = $('.input-body');
 var search = $('.input-search');
 var saveButton = $('.save-button');
+var defaultQuality = 'swill'
 
 saveButton.on('click', function() {
   createCards();
+  title.val('');
+  body.val('');
 });
 
 function createCards() {
-  $('.bottom-section').append(
+  $('.bottom-section').prepend(
     `<li class='card'>
     <h2 class='card-title'>${title.val()}</h2>
     <p class='card-body'>${body.val()}</p>
-    <p class='quality'></p>
+    <p class='quality'>quality: swill</p>
     <button class='upvote card-buttons'></button>
     <button class='downvote card-buttons'></button>
     <button class='delete card-buttons'></button>
@@ -22,4 +25,18 @@ function createCards() {
 
 $('.bottom-section').on('click', '.delete', function() {
   $(this).closest('li').remove();
-})
+});
+
+
+$('.bottom-section').on('click', '.upvote', function() {
+   var quality = $(this).siblings('.quality');
+   var selector = $(quality).val('');
+   $(quality).text('quality: swill');
+});
+
+
+// function upVote() {
+// switch (selector) {
+//   case 'quality: plausible'
+//   }
+// }

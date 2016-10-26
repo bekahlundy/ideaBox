5dce9f130587a9098ee5c806799edb0600bf6ba7
bekahlundy <lundyrebekah@gmail.com>
2016-10-26 11:37:09 -0600

    new js for master. update

--- javascript.js
+++ javascript.js
@@ -1,17 +1,5 @@
-<<<<<<< HEAD
 function Idea(title, body, id) {
   this.title= title;
-=======
-var $title = $('.input-title');
-var $body = $('.input-body');
-var $search = $('.input-search');
-var $saveButton = $('.save-button');
-
-checkIdeas();
-
-function Idea(title, body) {
-  this.title = title;
->>>>>>> master
   this.body = body;
   this.id = id;
   this.quality = 'swill';
@@ -39,7 +27,6 @@ function createCard(idea) {
     </li>`
   )
 };
-
 
 $('.bottom-section').on('click', '.upvote', '.downvote', function() {
   var selector = $(this).attr('class');
@@ -79,35 +66,6 @@ function getNewQuality(selector, quality) {
   }
 }
 
-$saveButton.on('click', function() {
-  var title = $title.val();
-  var body = $body.val();
-  var newIdea = new Idea(title, body); //new instance of Idea
-  storeIdea(newIdea);
-  createCard(newIdea);
-  $title.val('');
-  $body.val('');
-});
-
-function storeIdea(newIdea) {
-  var allIdeas = getIdeasFromLocalStorage();
-  allIdeas.push(newIdea);
-  localStorage.setItem("ideas", JSON.stringify(allIdeas))
-};
-
-function getIdeasFromLocalStorage() {
-  return JSON.parse(localStorage.getItem("ideas"))
-};
-
-function getAllIdeasAndDisplay() {
-  // grab from localStorage, iterate through ideas, for each idea call on createCard();
-  var ideas = JSON.parse(localStorage.getItem("ideas"));
-  for (var i = 0; ideas.length>i; i++) {
-    createCard(ideas[i]);
-  }
-};
->>>>>>> master
-
 function upVote(quality) {
   switch (quality) {
     case 'swill':
@@ -117,7 +75,6 @@ function upVote(quality) {
   }
 }
 
-<<<<<<< HEAD
 function downVote(quality) {
   switch (quality) {
     case 'genius':
@@ -131,14 +88,3 @@ function emptyInputs() {
   $('.input-title').val('');
   $('.input-body').val('');
 }
-=======
-$('.bottom-section').on('click', '.delete', function() {
-  $(this).closest('li').remove();
-});
-
-$('.bottom-section').on('click', '.upvote', function() {
-  var quality = $(this).siblings('.quality');
-  var selector = $(quality).val('');
-  $(quality).text('quality: swill');
-});
->>>>>>> master

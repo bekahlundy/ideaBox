a6e3b92747adfae4946c83add8b0f68717bc5a2e
pescel <28l.pesce@gmail.com>
2016-10-25 20:03:08 -0600

    Add upvote/downvote button and local storage functions

--- javascript.js
+++ javascript.js
@@ -8,15 +8,6 @@ function Idea(title, body, id) {
 }
 
 
-$('.save-button').on('click', function() {
-  var title = $('.input-title').val();
-  var body = $('.input-body').val();
-  var ideaBox = new Idea(title, body, Date.now());
-  var key = ideaBox.id;
-  localStorage.setItem(key, JSON.stringify(ideaBox))
-  createCard(title, body, ideaBox.id, ideaBox.quality);
-  emptyInputs();
-})
 
 
 function createCard(idea) {
@@ -31,6 +22,39 @@ function createCard(idea) {
     </li>`
   )
 };
+
+$('.save-button').on('click', function() {
+  var title = $('.input-title').val();
+  var body = $('.input-body').val();
+  var ideaBox = new Idea(title, body, Date.now());
+  var key = ideaBox.id;
+  localStorage.setItem(key, JSON.stringify(ideaBox));
+  createCard(ideaBox);
+  emptyInputs();
+});
+
+$('.bottom-section').on('click', '.upvote, .downvote', function() {
+  var selector = $(this).attr('class');
+  var quality = $(this).closest('.card').find('.quality');
+  var newQuality = getNewQuality(selector, quality.text())
+})
+
+function getNewQuality(selector, quality) {
+  if(selector === 'upvote card-buttons') {
+    return upVote(quality);
+  } else {
+    return downVote(quality);
+  }
+}
+
+function upVote(quality) {
+  switch (quality) {
+    case 'swill';
+    return 'plausible';
+    case 'plausible';
+    return 'genius';
+  }
+}
 
 function emptyInputs() {
   $('.input-title').val('');
